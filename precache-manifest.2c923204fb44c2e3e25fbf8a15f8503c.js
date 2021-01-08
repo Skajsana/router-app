499b690f4ada2cc5194a9b386ca3ba9600c4c6fa
Katarzyna Zemsta - Piasecka <zemstapiasecka@gmail.com>
2021-01-08 11:12:16 +0100

    Updates

---
+++ precache-manifest.2c923204fb44c2e3e25fbf8a15f8503c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5a8f06f4f4c0b965e4",
+    "url": "/router-app/static/css/main.25d39f76.chunk.css"
+  },
+  {
+    "revision": "db5a8f06f4f4c0b965e4",
+    "url": "/router-app/static/js/main.db5a8f06.chunk.js"
+  },
+  {
+    "revision": "a00cdf71a3fa2c38bc91",
+    "url": "/router-app/static/js/runtime~main.a00cdf71.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/router-app/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/router-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/router-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/router-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "558c4f3ea528b8a00aa8b761ed9685f5",
+    "url": "/router-app/index.html"
+  }
+];
